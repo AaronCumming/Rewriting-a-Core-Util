8d9a22e5f4ca5ff4dca4f1b5d6224cfe51d081ae
AaronCumming <142943747+AaronCumming@users.noreply.github.com>
2025-10-28 04:55:10 +0000

    finished

--- report.md
+++ report.md
@@ -1,1 +1,23 @@
-hello
+For this assignment, your job is to pick a coreutil: https://www.gnu.org/software/coreutils/manual/coreutils.html and rewrite it.
+
+In particular, you are to:
+
+Pick a single coreutil
+Rewrite the base (no flag input) functionality without calling the original core util
+Implement 2 flags available on the base core util
+
+
+Your report should be a development log:
+
+
+
+1. What command did you implement?
+I implemented the echo command.
+
+
+2. What was a hurdle?
+Getting the conditionals just right.  I had to rewrite them a few times and I had to get the logical 'and' correct.
+
+
+3. What was easy?
+Grabbing the input for the flags part with the options was easy since I had already done it before.
---
+++ rewrite_echo.sh
@@ -0,0 +1,41 @@
+# Aaron Cumming
+# 10/27/2025
+# CS 310
+# Simple Program for rewriting echo
+
+
+# Main program                                            
+
+noNewLine=False
+escapes=False
+
+
+# Get the options
+while getopts ":ne" option; do
+   case $option in
+      n) # no new line
+         noNewLine=True;;
+      e) # interpret escape sequences
+         escapes=True;;
+   esac
+done
+
+
+# Shift away parsed options so $1 points to the input string
+shift $((OPTIND - 1))
+
+# get the input string
+input_str="$*"
+
+if [ "$noNewLine" == True ] && [ "$escapes" == True ]; then
+    printf "%b" "$input_str"
+
+elif [ "$noNewLine" == False ] && [ "$escapes" == True ]; then
+    printf "%b\n" "$input_str"
+
+elif [ "$noNewLine" == True ] && [ "$escapes" == False ]; then
+    printf "%s" "$input_str"
+
+else
+    printf "%s\n" "$input_str"
+fi
